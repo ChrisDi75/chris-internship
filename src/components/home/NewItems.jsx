57e341b0a7e431219be4cc9e3a2a1e1b5a72440c
Chris <ventox16@hotmail.de>
2023-11-15 14:25:00 +0100

    fetch data from api, dynamic display inside a dynamic carousel

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import axios from "axios";
+import OwlCarousel from "react-owl-carousel";
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
+import Skeleton from "../UI/Skeleton";
 
 const NewItems = () => {
+
+  const [items, setItems] = useState([]);
+  const [skeleton, setSkeleton] = useState(true);
+
+  const owlCarouselModifier = {
+    loop: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      900:{
+        items: 3,
+      },
+      1200:{
+        items: 4,
+      },
+    },
+  };
+
+  async function renderItems() {
+    setSkeleton(true);
+  const { data } = await axios.get(`https://us-central1-nft-cloud-functions.cloudfunctions.net/newItems`)
+  setItems(data);
+  setSkeleton(false);
+}
+useEffect(() => {
+  renderItems();
+  }, [])
+
   return (
     <section id="section-items" className="no-bottom">
       <div className="container">
@@ -14,22 +50,71 @@ const NewItems = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {new Array(4).fill(0).map((_, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+          {skeleton? (
+            new Array(1).fill(0).map((index) => (
+              <OwlCarousel {...owlCarouselModifier}>
+                <div key={index}>
+                  <div className="nft__item">
+                    <div className="author_list_pp">
+                      <Skeleton borderRadius={50} height={50} width={50} />
+                      <i className="fa fa-check"></i>
+                    </div>
+                    <div className="nft__item_wrap">
+                      <div className="nft__item_extra">
+                        <div className="nft__item_buttons">
+                          <button>Buy Now</button>
+                          <div className="nft__item_share">
+                            <h4>Share</h4>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-facebook fa-lg"></i>
+                            </a>
+                            <a href="" target="_blank" rel="noreferrer">
+                              <i className="fa fa-twitter fa-lg"></i>
+                            </a>
+                            <a href="">
+                              <i className="fa fa-envelope fa-lg"></i>
+                            </a>
+                          </div>
+                        </div>
+                      </div>
+                      <Skeleton height="95%" width="100%" />
+                    </div>
+                    <div className="nft__item_info">
+                      <h4>
+                        <Skeleton height={20} width={100} />
+                      </h4>
+                      <div className="nft__item_price">
+                        <Skeleton height={20} width={60} />
+                      </div>
+                      <div className="nft__item_like">
+                        <i className="fa fa-heart"></i>
+                        <span>
+                          <Skeleton height={20} width={50} />
+                        </span>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </OwlCarousel>
+                ))
+                )
+          :
+          (<OwlCarousel {...owlCarouselModifier} >
+          {items.map((item, index) => (
+            <div key={index}>
               <div className="nft__item">
                 <div className="author_list_pp">
                   <Link
-                    to="/author"
+                    to={`/author/${item.authorId}`}
                     data-bs-toggle="tooltip"
                     data-bs-placement="top"
                     title="Creator: Monica Lucas"
                   >
-                    <img className="lazy" src={AuthorImage} alt="" />
+                    <img className="lazy" src={item.authorImage} alt="" />
                     <i className="fa fa-check"></i>
                   </Link>
                 </div>
                 <div className="de_countdown">5h 30m 32s</div>
-
                 <div className="nft__item_wrap">
                   <div className="nft__item_extra">
                     <div className="nft__item_buttons">
@@ -48,28 +133,29 @@ const NewItems = () => {
                       </div>
                     </div>
                   </div>
-
-                  <Link to="/item-details">
+                  <Link to={`/item-details/${item.nftId}`}>
                     <img
-                      src={nftImage}
+                      src={item.nftImage}
                       className="lazy nft__item_preview"
                       alt=""
                     />
                   </Link>
                 </div>
                 <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
+                  <Link to={`/item-details/${item.nftId}`}>
+                    <h4>{item.title}</h4>
                   </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
+                  <div className="nft__item_price">{item.price} ETH</div>
                   <div className="nft__item_like">
                     <i className="fa fa-heart"></i>
-                    <span>69</span>
+                    <span>{item.likes}</span>
                   </div>
                 </div>
               </div>
             </div>
           ))}
+          </OwlCarousel>
+          )}
         </div>
       </div>
     </section>
